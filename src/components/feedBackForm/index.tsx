61b54199279cf05c418bb06d20b275c9827d6725
Pavel <pde0177776@gmail.com>
2025-02-05 12:51:27 +0400

    Добавил форму обртаной связи

---
+++ src/components/feedBackForm/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import cl from "./feedBackForm.module.css"
+const FeedBackForm = () => {
+    return (
+        <form className={cl.form}>
+            <div className={cl.form__group}>
+                <input
+                    className={cl.form__field}
+                    placeholder={"Имя и Фамилия"}
+                    type="text"
+                    id="name"
+                />
+                <label htmlFor="name" className={cl.form__label}>Имя и Фамилия</label>
+            </div>
+            <div className={cl.form__group}>
+                <input
+                    className={cl.form__field}
+                    placeholder={"Откуда вы ?"}
+                    type="text"
+                    id="from"
+                />
+                <label htmlFor="from" className={cl.form__label}>Откуда вы ?</label>
+            </div>
+            <div className={cl.form__group}>
+                <textarea
+                    className={cl.form__field}
+                    style={{fontSize: "1.3rem", minHeight: "200px"}}
+                    placeholder={"Расскажите свою историю (какой аромат напоминает вам родные места?)"}
+                    id="from"
+                />
+                <label htmlFor="from" className={cl.form__label}>Расскажите свою историю (какой аромат напоминает вам
+                    родные места?)</label>
+            </div>
+            <div className={cl.form__group}>
+                <input
+                    className={cl.form__field}
+                    placeholder={"Как с вами связаться? (телефон, e-mail или соцсети)"}
+                    type="text"
+                    id="connection"
+                />
+                <label htmlFor="connection" className={cl.form__label}>Как с вами связаться?</label>
+            </div>
+            <button className={cl.form__btn}>
+                Отправить
+            </button>
+        </form>
+    );
+};
+
+export default FeedBackForm;
--- src/pages/home/homePage.tsx
+++ src/pages/home/homePage.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import cl from "./home.module.css"
-import Icon from "../../components/Icon";
+import w1 from "../../media/img/w1.png"
+import w3 from "../../media/img/w3.jpg"
+import w4 from "../../media/img/w4.jpg"
+import FeedBackForm from "../../components/feedBackForm";
 const HomePage = () => {
     return (
-        <div>
-        </div>
+        <section className={cl.home}>
+            {/*<h2 className={cl.title}>О компании</h2>*/}
+            {/*<div className={cl.about__list}>*/}
+            {/*    <div className={cl.list__item}>*/}
+            {/*        <img className={cl.item__img} src={w1} alt=""/>*/}
+            {/*        <p className={cl.item__text}>*/}
+            {/*            Бренд основан молодой активной девушкой, вдохновленной идеей создать новый сегмент*/}
+            {/*            среди уходовых средств для дома, разработанных на основе: инноваций, осмысленного потребления,*/}
+            {/*            заботе о самом ценном.*/}
+            {/*        </p>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            <FeedBackForm/>
+        </section>
     );
 };
 
