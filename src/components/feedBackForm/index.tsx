fc6135e5c6fa4363976b623c6ded52f7d62931b2
Pavel <pde0177776@gmail.com>
2025-02-06 09:44:55 +0400

    Добавил отправку письма на почту

--- src/components/feedBackForm/index.tsx
+++ src/components/feedBackForm/index.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import cl from "./feedBackForm.module.css"
 const FeedBackForm = () => {
+
+    const onSubmit = async (event:any) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "e5615027-944a-46b3-a4d0-efb21d9e5892");
+
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+
+        const res = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: json
+        }).then((res) => res.json());
+
+        if (res.success) {
+            console.log("Success", res);
+        }
+    };
+
+
     return (
-        <form className={cl.form}>
+        <form className={cl.form} onSubmit={onSubmit}>
             <div className={cl.form__group}>
                 <input
                     className={cl.form__field}
                     placeholder={"Имя и Фамилия"}
                     type="text"
                     id="name"
+                    name="name"
                 />
                 <label htmlFor="name" className={cl.form__label}>Имя и Фамилия</label>
             </div>
@@ -18,6 +44,7 @@ const FeedBackForm = () => {
                     placeholder={"Откуда вы ?"}
                     type="text"
                     id="from"
+                    name="from"
                 />
                 <label htmlFor="from" className={cl.form__label}>Откуда вы ?</label>
             </div>
@@ -27,6 +54,7 @@ const FeedBackForm = () => {
                     style={{fontSize: "1.3rem", minHeight: "200px"}}
                     placeholder={"Расскажите свою историю (какой аромат напоминает вам родные места?)"}
                     id="from"
+                    name="story"
                 />
                 <label htmlFor="from" className={cl.form__label}>Расскажите свою историю (какой аромат напоминает вам
                     родные места?)</label>
@@ -37,10 +65,11 @@ const FeedBackForm = () => {
                     placeholder={"Как с вами связаться? (телефон, e-mail или соцсети)"}
                     type="text"
                     id="connection"
+                    name="connection"
                 />
                 <label htmlFor="connection" className={cl.form__label}>Как с вами связаться?</label>
             </div>
-            <button className={cl.form__btn}>
+            <button type="submit" className={cl.form__btn}>
                 Отправить историю
             </button>
         </form>
